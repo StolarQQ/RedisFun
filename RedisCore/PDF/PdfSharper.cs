2a7bd13c8e174039a209c068935791f270f0efbe
Stolar <stolar28@gmail.com>
2019-09-25 22:12:54 +0200

    Pdf checker

---
+++ RedisCore/PDF/PdfSharper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfDocument = PdfSharp.Pdf.PdfDocument;
+using PdfReader = PdfSharp.Pdf.IO.PdfReader;
+
+
+namespace RedisCore.PDF
+{
+    public class PdfSharper
+    {
+        public void MergePdfs(string[] inputs)
+        {
+            var outPdfDocument = new PdfDocument();
+
+            foreach (var pdf in inputs)
+            {
+                var inputDocument = PdfReader.Open(pdf, PdfDocumentOpenMode.Import);
+                outPdfDocument.Version = inputDocument.Version;
+
+                foreach (var page in inputDocument.Pages)
+                {
+                    outPdfDocument.AddPage(page);
+                }
+            }
+
+            var name = "WTF.pdf";
+            outPdfDocument.Info.Creator = string.Empty;
+            outPdfDocument.Save($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/lens.pdf"); 
+
+            Process.Start(name);
+        }
+
+        public void MergePdfsIText(string[] inputs)
+        {
+            var outPdfDocument = new PdfDocument();
+
+            foreach (var pdf in inputs)
+            {
+                var inputDocument = PdfReader.Open(pdf, PdfDocumentOpenMode.Import);
+                outPdfDocument.Version = inputDocument.Version;
+
+                foreach (var page in inputDocument.Pages)
+                {
+                    outPdfDocument.AddPage(page);
+                }
+            }
+
+            var name = "WTF.pdf";
+            outPdfDocument.Info.Creator = string.Empty;
+            //outPdfDocument.Info.Producer = string.Empty;
+
+            outPdfDocument.Save($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/lens.pdf");
+
+            Process.Start(name);
+        }
+
+        public byte[] CreatePdf(byte inputs)
+        {
+            var document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            var gtx = XGraphics.FromPdfPage(page);
+            var font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            gtx.DrawString("Hello, World!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+                XStringFormats.Center);
+
+            string fileName = "HelloWorld.pdf";
+            document.Save(fileName);
+
+            return new byte[1];
+        }
+
+        public bool IsPdf(byte[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPdf
+    {
+        byte[] MergePdfs(byte[] inputs);
+        //byte[] CreatePdf(byte inputs);
+        //bool IsPdf(byte[] inputs);
+    }
+}
--- RedisCore/Program.cs
+++ RedisCore/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using PdfSharp.Pdf;
 using StackExchange.Redis;
 
 namespace RedisCore
@@ -10,7 +14,77 @@ namespace RedisCore
     {
         static async Task Main(string[] args)
         {
+            //await RedisInit();
+            // var fs = new FileStream($"{desktopPath}/xD1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            //var openpdf = PdfReader.Open(inStream, PdfDocumentOpenMode.Import);
 
+
+
+            var bytePdf = new byte[1];
+            var inStream = new MemoryStream(bytePdf);
+            File.WriteAllBytes("C:\\Users\\Lyns\\Desktop\\Test123.pdf", bytePdf);
+
+            // Simple check if file is pdf
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Console.WriteLine(IsPdf($"{desktopPath}/Test123.pdf"));
+
+
+            // Create blank pdf
+            var stream = new MemoryStream();
+            var pdf = new PdfDocument();
+            var page = pdf.AddPage();
+            pdf.Save(stream, false);
+            pdf.Close();
+            var resultPdf = stream.ToArray();
+
+            // Write pdf
+            File.WriteAllBytes("C:\\Users\\Lyns\\Desktop\\Stolar.pdf", resultPdf);
+
+
+
+
+
+
+            //File.WriteAllBytes($"{desktopPath}/Rafał.pdf", pdf);
+
+            //File.ReadAllBytes("");
+
+
+            //var pdfSharper = new PdfSharper();
+            //pdfSharper.MergePdfs(new[]
+            //{
+            //    $"{desktopPath}/xD.pdf",
+            //    $"{desktopPath}/ReSharper_DefaultKeymap_VSscheme.pdf"
+
+            //});
+
+            Console.ReadKey();
+        }
+
+        static bool IsPdf(string path)
+        {
+            var pdfString = "%PDF-";
+            var pdfBytes = Encoding.ASCII.GetBytes(pdfString);
+            var len = pdfBytes.Length;
+            var buf = new byte[len];
+            var remaining = len;
+            var pos = 0;
+
+            using (var f = File.OpenRead(path))
+            {
+                while (remaining > 0)
+                {
+                    var amtRead = f.Read(buf, pos, remaining);
+                    if (amtRead == 0) return false;
+                    remaining -= amtRead;
+                    pos += amtRead;
+                }
+            }
+            return pdfBytes.SequenceEqual(buf);
+        }
+
+        private static async Task RedisInit()
+        {
             var redisService = new Redis();
             await redisService.SetCache("name", "Andrew");
             var data = await redisService.GetCache("name");
@@ -25,8 +99,6 @@ namespace RedisCore
             Console.WriteLine(employeeFromCache.Id);
             Console.WriteLine(employeeFromCache.Name);
             Console.WriteLine(employeeFromCache.Age);
-
-            Console.ReadKey();
         }
     }
 
