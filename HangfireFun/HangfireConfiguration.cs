cccfa9ac36b10fe2da881481ba7c6e0f2ca17c6b
Stolar <stolar28@gmail.com>
2019-09-26 21:26:37 +0200

    Add hangfire dll with init

---
+++ HangfireFun/HangfireConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+using Owin;
+
+namespace HangfireFun
+{
+    public static class HangfireConfiguration  
+    {
+        public static void HangfireInit(string database, IAppBuilder app)
+        {
+
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(database,
+                 new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1)});
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
